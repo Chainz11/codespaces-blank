972baef81668c4e54ad5a37e8673de824948e564
Chainz11 <134762985+Chainz11@users.noreply.github.com>
2023-05-29 11:30:15 +0000

    new file:   29.05.2023/zad.1/Program.cs 	new file:   29.05.2023/zad.1/bin/Debug/net7.0/zad.1 	new file:   29.05.2023/zad.1/bin/Debug/net7.0/zad.1.deps.json 	new file:   29.05.2023/zad.1/bin/Debug/net7.0/zad.1.dll 	new file:   29.05.2023/zad.1/bin/Debug/net7.0/zad.1.pdb 	new file:   29.05.2023/zad.1/bin/Debug/net7.0/zad.1.runtimeconfig.json 	new file:   29.05.2023/zad.1/obj/Debug/net7.0/.NETCoreApp,Version=v7.0.AssemblyAttributes.cs 	new file:   29.05.2023/zad.1/obj/Debug/net7.0/apphost 	new file:   29.05.2023/zad.1/obj/Debug/net7.0/ref/zad.1.dll 	new file:   29.05.2023/zad.1/obj/Debug/net7.0/refint/zad.1.dll 	new file:   29.05.2023/zad.1/obj/Debug/net7.0/zad.1.AssemblyInfo.cs 	new file:   29.05.2023/zad.1/obj/Debug/net7.0/zad.1.AssemblyInfoInputs.cache 	new file:   29.05.2023/zad.1/obj/Debug/net7.0/zad.1.GeneratedMSBuildEditorConfig.editorconfig 	new file:   29.05.2023/zad.1/obj/Debug/net7.0/zad.1.GlobalUsings.g.cs 	new file:   29.05.2023/zad.1/obj/Debug/net7.0/zad.1.assets.cache 	new file:   29.05.2023/zad.1/obj/Debug/net7.0/zad.1.csproj.AssemblyReference.cache 	new file:   29.05.2023/zad.1/obj/Debug/net7.0/zad.1.csproj.CoreCompileInputs.cache 	new file:   29.05.2023/zad.1/obj/Debug/net7.0/zad.1.csproj.FileListAbsolute.txt 	new file:   29.05.2023/zad.1/obj/Debug/net7.0/zad.1.dll 	new file:   29.05.2023/zad.1/obj/Debug/net7.0/zad.1.genruntimeconfig.cache 	new file:   29.05.2023/zad.1/obj/Debug/net7.0/zad.1.pdb 	new file:   29.05.2023/zad.1/obj/project.assets.json 	new file:   29.05.2023/zad.1/obj/project.nuget.cache 	new file:   29.05.2023/zad.1/obj/zad.1.csproj.nuget.dgspec.json 	new file:   29.05.2023/zad.1/obj/zad.1.csproj.nuget.g.props 	new file:   29.05.2023/zad.1/obj/zad.1.csproj.nuget.g.targets 	new file:   29.05.2023/zad.1/zad.1.csproj 	new file:   29.05.2023/zad.2/Program.cs 	new file:   29.05.2023/zad.2/bin/Debug/net7.0/zad.2 	new file:   29.05.2023/zad.2/bin/Debug/net7.0/zad.2.deps.json new file:   29.05.2023/zad.2/bin/Debug/net7.0/zad.2.dll new file:   29.05.2023/zad.2/bin/Debug/net7.0/zad.2.pdb 	new file:   29.05.2023/zad.2/bin/Debug/net7.0/zad.2.runtimeconfig.json 	new file:   29.05.2023/zad.2/obj/Debug/net7.0/.NETCoreApp,Version=v7.0.AssemblyAttributes.cs 	new file:   29.05.2023/zad.2/obj/Debug/net7.0/apphost 	new file:   29.05.2023/zad.2/obj/Debug/net7.0/ref/zad.2.dll 	new file:   29.05.2023/zad.2/obj/Debug/net7.0/refint/zad.2.dll new file:   29.05.2023/zad.2/obj/Debug/net7.0/zad.2.AssemblyInfo.cs 	new file:   29.05.2023/zad.2/obj/Debug/net7.0/zad.2.AssemblyInfoInputs.cache 	new file:   29.05.2023/zad.2/obj/Debug/net7.0/zad.2.GeneratedMSBuildEditorConfig.editorconfig 	new file:   29.05.2023/zad.2/obj/Debug/net7.0/zad.2.GlobalUsings.g.cs 	new file:   29.05.2023/zad.2/obj/Debug/net7.0/zad.2.assets.cache 	new file:   29.05.2023/zad.2/obj/Debug/net7.0/zad.2.csproj.AssemblyReference.cache 	new file:   29.05.2023/zad.2/obj/Debug/net7.0/zad.2.csproj.CoreCompileInputs.cache 	new file:   29.05.2023/zad.2/obj/Debug/net7.0/zad.2.csproj.FileListAbsolute.txt 	new file:   29.05.2023/zad.2/obj/Debug/net7.0/zad.2.dll 	new file:   29.05.2023/zad.2/obj/Debug/net7.0/zad.2.genruntimeconfig.cache 	new file:   29.05.2023/zad.2/obj/Debug/net7.0/zad.2.pdb 	new file:   29.05.2023/zad.2/obj/project.assets.json 	new file:   29.05.2023/zad.2/obj/project.nuget.cache 	new file:   29.05.2023/zad.2/obj/zad.2.csproj.nuget.dgspec.json 	new file:   29.05.2023/zad.2/obj/zad.2.csproj.nuget.g.props 	new file:   29.05.2023/zad.2/obj/zad.2.csproj.nuget.g.targets 	new file:   29.05.2023/zad.2/zad.2.csproj 	29.05.2023/zad.3/

---
+++ 29.05.2023/zad.1/Program.cs
@@ -0,0 +1,11 @@
+﻿///masivi
+
+int [] arr =
+    {
+        10,18,-52,4,2
+    };
+
+foreach (int number in arr )
+{
+    Console.WriteLine(number);
+}
---
+++ 29.05.2023/zad.2/Program.cs
@@ -0,0 +1,21 @@
+﻿//Masivi 
+int[] arr = new int[5]; //inicializirane na masiv
+
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine($"Enter element[{i}]=");
+    arr[i] = int.Parse(Console.ReadLine());
+}
+
+Console.WriteLine("------ foreach ------");
+
+foreach (int number in arr)
+{
+    Console.WriteLine(number);
+}
+
+Console.WriteLine("------ for ------");
+for (int i = 0; i < arr.Length; i++)
+{
+    Console.WriteLine($" arr[{i}] = {arr[i]}");
+}
